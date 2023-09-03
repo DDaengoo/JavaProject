7d140a1418d95708da9774cb7c6580aa06e0bdcf
Yang HyunWoo <mynameisyiombi@gmail.com>
2023-09-03 14:24:19 +0900

    1st commit
    make CRUD create part

---
+++ WordMasterProject/src/com/mycom/word/Main.java
@@ -0,0 +1,17 @@
+package com.mycom.word;
+
+public class Main {
+    public static void main(String[] args) {
+        new WordManeger().start();
+    }
+
+}
+
+
+/*
+=> 난이도(1,2,3) & 새 단어 입력 :
+뜻 입력 :
+새단어가 단어장에 추가되었습니다.
+*/
+
+
